b24cdbccc602b54306f08c0d8704aa2392f3b4f8
Candyce10 <candycematthews@yahoo.com>
2022-04-08 13:40:32 -0700

    finished javascript questions

---
+++ trivia.js
@@ -0,0 +1,32 @@
+let questions = [
+    {question: "What was the bestselling video game console of the 1990's?",
+    answer1: "Game Boy Color",
+    answer2: "Nintendo64",
+    answer3: "Playstation",
+    answer4: "Dreamcast"
+},
+    {question: "What was the highest grossing film of the 90's?", 
+    answer1: "Titanic",
+    answer2: "Star Wars",
+    answer3: "Jurassic Park",
+    answer4: "Forrest Gump"
+},
+    {question: "What was the most popular way to communicate in the 90's?",
+    answer1: "E-mail",
+    answer2: "Pagers",
+    answer3: "Talk Over The Phone",
+    answer4: "Hand-written Mail"
+},
+    {question: "When was the World Wide Web first introduced?",
+    answer1: "1990", 
+    answer2: "1991", 
+    answer3: "1992", 
+    answer4: "1993"
+},
+    {question: "Which toy was originally feared to be a tool for foreign spies?",
+    answer1: "Talk Boy",
+    answer2: "Tickle Me Elmo",
+    answer3: "Game Boy Color",
+    answer4: "Furby", 
+}
+]
